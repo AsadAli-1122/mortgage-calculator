f0eca5fd64dc743b6d040719ce708428aa35d19f
AsadAli-1122 <asadali.cool.23@gmail.com>
2025-03-01 20:54:33 +0500

    adjust image size

--- src/components/EmptyResult.tsx
+++ src/components/EmptyResult.tsx
@@ -7,7 +7,7 @@ const EmptyResult: React.FC = () => {
       id="initialState"
       className="w-full h-full flex flex-col justify-center items-center"
     >
-      <Image width={500} height={500} src="/images/illustration-empty.svg" alt="illustration-empty" />
+      <Image width={500} height={500} src="/images/illustration-empty.svg" alt="illustration-empty" className="max-w-52" />
       <h2 className="text-2xl font-semibold mt-2">Results Shown here</h2>
       <p className="text-sm text-[#5E7B8C] mt-2 text-center">
         Complete the form and click &quot;calculate repayments&quot; to see what your
